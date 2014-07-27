f1d6ca2e5a2821a3bd9ff57fbd7de1eb8e93510e
David Fries <David@Fries.net>
2014-07-27 13:09:38 -0500

    keep all the printable characters in ascii
    This way just the non-printable ones are discarded.

--- src/exiftool.js
+++ src/exiftool.js
@@ -1509,7 +1509,10 @@
             }
             str = str + "";
 
-            str = str.replace(/[^a-z0-9 \-\/\.\(\)\:\;\,\©\@\\]/gi, '');
+            // list of what to keep
+            str = str.replace(
+/[^a-z0-9 \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~\©]/gi,
+                '');
             str = str.replace(/^\s+|\s+$/g, ''); // trim
             if (str.toLowerCase() == "undefined"
                     || str.toLowerCase() == "unknown") {
